216a0f578c6a2c3228d8a900193a1e158f18108a
Pierre-Henry Soria <hi@ph7.me>
2021-11-15 08:03:19 +1030

    Remove redundant docBlock and indentation

--- src/Action/User/UserAction.php
+++ src/Action/User/UserAction.php
@@ -10,18 +10,9 @@ use Psr\Log\LoggerInterface;
 
 abstract class UserAction extends Action
 {
-    /**
-     * @var UserRepository
-     */
-    protected $userRepository;
+    protected UserRepository $userRepository;
 
-    /**
-     * @param LoggerInterface $logger
-     * @param UserRepository $userRepository
-     */
-    public function __construct(LoggerInterface $logger,
-                                UserRepository $userRepository
-    )
+    public function __construct(LoggerInterface $logger, UserRepository $userRepository)
     {
         parent::__construct($logger);
         $this->userRepository = $userRepository;
