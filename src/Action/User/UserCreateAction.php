dbfa151160e1cbdd94ff27fd30760eddf9db3bdb
Pierre-Henry Soria <hi@ph7.me>
2021-11-15 08:02:07 +1030

    Add property type

--- src/Action/User/UserCreateAction.php
+++ src/Action/User/UserCreateAction.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\ServerRequestInterface;
 
 final class UserCreateAction
 {
-    private object $userCreator;
+    private UserCreator $userCreator;
 
     public function __construct(UserCreator $userCreator)
     {
